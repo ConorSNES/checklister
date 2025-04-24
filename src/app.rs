f19ce09a18131fb29fa3d8ff386692170a1a3ad6
Conor <181732605+ConorSS@users.noreply.github.com>
2025-04-24 16:27:11 +0100

    Basic layout

--- src/app.rs
+++ src/app.rs
@@ -1,53 +1,148 @@
-use eframe::egui;
-use model::{Entry, Model};
+use eframe::egui::{self, Color32, Frame, Layout, ScrollArea, Ui};
+use model::{Entry, EntryHost, EntrySwitch, Model};
 use serde::{Deserialize, Serialize};
 mod model;
 
 enum Action {
-	Add{loc: Vec<isize>, dat: Entry},
-	Remove{loc: Vec<isize>}
+    Add { loc: Vec<isize>, dat: Entry },
+    Remove { loc: Vec<isize> },
+}
+
+#[derive(Serialize, Deserialize)]
+enum CurrentMenu {
+    TableView,
+    Create,
 }
 
 // declaration of the application
 #[derive(Serialize, Deserialize)]
 pub struct App {
-	body : String,
-	data : Model
+    body: String,
+    data: Model,
+    menu: CurrentMenu,
 }
 
 impl Default for App {
-	fn default() -> Self {
-		Self {
-			body : "Hello, world".to_owned(),
-			data : Model::default()
-		}
-	}
+    fn default() -> Self {
+        Self {
+            body: "Hello, world".to_owned(),
+            data: model::make_sample_set(),
+            menu: CurrentMenu::TableView,
+        }
+    }
 }
 
 impl App {
-	fn tableview() {
-		
-	}
+    const COL_WHITE: Color32 = Color32::from_rgb(0xFF, 0xFF, 0xFF); //::from_hex("#fff").expect("White is invalid");
+    const COL_GREY10: Color32 = Color32::from_rgb(0xEE, 0xEE, 0xEE); //::from_hex("#eee").expect("Grey10 is invalid");
+
+    // Constructs view of all elements.
+    fn tableview(&self, ui: &mut Ui) {
+        ui.with_layout(
+            Layout::top_down(egui::Align::Min).with_main_justify(true),
+            |ui| {
+                ScrollArea::vertical().show(ui, |ui| {
+                    Frame::new().show(ui, |ui| {
+                        Self::drawentryhost(ui, &self.data.entry, false);
+                    })
+                });
+            },
+        );
+    }
+
+    // Wraps many drawentry(s) together.
+    fn drawentryhost(ui: &mut Ui, subject: &EntryHost, displayeven: bool) -> (usize, usize) {
+        let mut o: (usize, usize) = (0, 0);
+
+        for v in &subject.subelements {
+            let pending = Self::drawentry(ui, v, displayeven);
+            o.0 += pending.0;
+            o.1 += pending.1;
+        }
+
+        o
+    }
+
+    // Dictates how entry should be drawn.
+    fn drawentry(ui: &mut Ui, subject: &Entry, displayeven: bool) -> (usize, usize) {
+        let mut o = (0, 0);
+
+        Frame::new()
+            .fill(match displayeven {
+                false => Self::COL_WHITE,
+                true => Self::COL_GREY10,
+            })
+            .corner_radius(4)
+            .inner_margin(4)
+            .show(ui, |ui| {
+                // Switch depending on subject.
+                if let EntrySwitch::End(v) = &subject.data {
+                    // This is a node of the tree with full data.
+                    ui.with_layout(Layout::left_to_right(egui::Align::Min), |ui| {
+                        // Display full data.
+                        if ui
+                            .button(match v.completed {
+                                None => " ",
+                                Some(_) => "•",
+                            })
+                            .clicked()
+                        {
+                            // When clicked, toggle completed state.
+                            // DAMMIT
+                        }
+
+                        // Add to o.
+                        if let Some(comp) = v.completed {
+                            o.0 += 1;
+                            ui.label(comp.format("%d/%m/%Y").to_string());
+                        } else {
+                            o.1 += 1;
+                        }
+
+                        ui.label(subject.title.to_owned());
+                    });
+                } else if let EntrySwitch::Host(v) = &subject.data {
+                    // This is a node of the tree with partial data.
+                    ui.with_layout(Layout::top_down(egui::Align::Min), |ui| {
+                        // Draw header with title.
+                        ui.label(subject.title.to_owned());
+
+                        // Go recursive using host.
+                        let totals = Self::drawentryhost(ui, v, !displayeven);
+
+                        // Merge pending totals with output totals.
+                        o.0 += totals.0;
+                        o.1 += totals.1;
+
+                        // Draw footer in small text.
+                        ui.label("footer");
+                    });
+                }
+            });
+
+        o
+    }
 }
 
 impl eframe::App for App {
-	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-		// construct navpanel
-		egui::SidePanel::left("navigation").show(ctx, |ui| {
-			ui.vertical_centered(|ui| {
-				if ui.button("Table view").clicked() {
-					self.menu = CurrentMenu::TableView;
-				}
-				if ui.button("New entry").clicked() {
-					self.menu = CurrentMenu::Create;
-				};
-				ui.set_width(32.0);
-			});
-		});
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // construct navpanel
+        egui::SidePanel::left("navigation").show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                if ui.button("Table view").clicked() {
+                    self.menu = CurrentMenu::TableView;
+                }
+                if ui.button("New entry").clicked() {
+                    self.menu = CurrentMenu::Create;
+                };
+                ui.set_width(32.0);
+            });
+        });
 
-		// construct body
-		egui::CentralPanel::default().show(ctx, |ui| {
-			ui.heading(self.body.clone());
-		});
-	}
+        // construct body
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(self.body.clone());
+            self.tableview(ui);
+        });
+    }
 }
--- src/app/model/mod.rs
+++ src/app/model/mod.rs
@@ -3,31 +3,108 @@
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
+pub fn make_sample_set() -> Model {
+    Model {
+        entry: EntryHost {
+            subelements: vec![
+                Entry {
+                    title: "snusk".to_owned(),
+                    data: EntrySwitch::End(EntryEnd {
+                        body: "bjarg".to_owned(),
+                        added: NaiveDateTime::default(),
+                        completed: None,
+                    }),
+                },
+                Entry {
+                    title: "snusk2".to_owned(),
+                    data: EntrySwitch::End(EntryEnd {
+                        body: "bjarg".to_owned(),
+                        added: NaiveDateTime::default(),
+                        completed: Some(NaiveDateTime::default()),
+                    }),
+                },
+                Entry {
+                    title: "snusk3".to_owned(),
+                    data: EntrySwitch::End(EntryEnd {
+                        body: "bjarg".to_owned(),
+                        added: NaiveDateTime::default(),
+                        completed: None,
+                    }),
+                },
+                Entry {
+                    title: "snusklist".to_owned(),
+                    data: EntrySwitch::Host(EntryHost {
+                        subelements: vec![
+							Entry {
+								title: "subsnusk1".to_owned(),
+								data: EntrySwitch::End(EntryEnd {
+									body: "bjarg".to_owned(),
+									added: NaiveDateTime::default(),
+									completed: Some(NaiveDateTime::default()),
+								}),
+							},
+							Entry {
+								title: "subsnusk2".to_owned(),
+								data: EntrySwitch::End(EntryEnd {
+									body: "bjarg".to_owned(),
+									added: NaiveDateTime::default(),
+									completed: None,
+								}),
+							},
+							Entry {
+								title: "subsnusk3".to_owned(),
+								data: EntrySwitch::End(EntryEnd {
+									body: "bjarg".to_owned(),
+									added: NaiveDateTime::default(),
+									completed: Some(NaiveDateTime::default()),
+								}),
+							}
+						],
+                    }),
+                },
+				Entry {
+                    title: "snusk4".to_owned(),
+                    data: EntrySwitch::End(EntryEnd {
+                        body: "bjarg".to_owned(),
+                        added: NaiveDateTime::default(),
+                        completed: None,
+                    }),
+                },
+            ],
+        },
+    }
+}
+
+// The data model contains only one entry host.
 #[derive(Default, Serialize, Deserialize)]
 pub struct Model {
-	pub entry : EntryHost
+    pub entry: EntryHost,
 }
 
+// Used to describe the switch of entries.
 #[derive(Serialize, Deserialize)]
 pub enum EntrySwitch {
-	End(EntryEnd),
-	Host(EntryHost),
+    End(EntryEnd),
+    Host(EntryHost),
 }
 
+// An entry contains either an array of entries or the main entry data.
 #[derive(Serialize, Deserialize)]
 pub struct Entry {
-	pub title: String,
-	pub data: EntrySwitch
+    pub title: String,
+    pub data: EntrySwitch,
 }
 
+// The main content of an entry.
 #[derive(Serialize, Deserialize)]
 pub struct EntryEnd {
-	pub body: String,
-	pub added: NaiveDateTime,
-	pub completed: Option<NaiveDateTime>
+    pub body: String,
+    pub added: NaiveDateTime,
+    pub completed: Option<NaiveDateTime>,
 }
 
+// An array of entries.
 #[derive(Serialize, Deserialize, Default)]
 pub struct EntryHost {
-	pub subelements: Vec<Entry>
+    pub subelements: Vec<Entry>,
 }
