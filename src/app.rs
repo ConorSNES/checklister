32573dbdd846cd5052680a3a56316b4156ed8ebc
Conor <181732605+ConorSS@users.noreply.github.com>
2025-04-25 09:30:57 +0100

    Main pane now fits full width

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{self, Color32, Frame, Layout, ScrollArea, Ui};
+use eframe::egui::{self, Button, Color32, Frame, Label, Layout, ScrollArea, Theme, Ui};
 use model::{Entry, EntryHost, EntrySwitch, Model};
 use serde::{Deserialize, Serialize};
 mod model;
@@ -34,20 +34,20 @@ impl Default for App {
 
 impl App {
     const COL_WHITE: Color32 = Color32::from_rgb(0xFF, 0xFF, 0xFF); //::from_hex("#fff").expect("White is invalid");
-    const COL_GREY10: Color32 = Color32::from_rgb(0xEE, 0xEE, 0xEE); //::from_hex("#eee").expect("Grey10 is invalid");
+    const COL_GREY10: Color32 = Color32::from_rgb(0xF8, 0xF8, 0xF8); //::from_hex("#eee").expect("Grey10 is invalid");
 
     // Constructs view of all elements.
     fn tableview(&self, ui: &mut Ui) {
-        ui.with_layout(
-            Layout::top_down(egui::Align::Min).with_main_justify(true),
-            |ui| {
-                ScrollArea::vertical().show(ui, |ui| {
-                    Frame::new().show(ui, |ui| {
-                        Self::drawentryhost(ui, &self.data.entry, false);
-                    })
-                });
-            },
-        );
+		ScrollArea::vertical().show(ui, |ui| {
+			Frame::new().show(ui, |ui| {
+				// Start the first drawentryhost on internal (root) data.
+				Self::drawentryhost(ui, &self.data.entry, false);
+			})
+		});
+
+        ui.with_layout(Layout::top_down(egui::Align::Min), |ui| {
+            
+        });
     }
 
     // Wraps many drawentry(s) together.
@@ -66,82 +66,95 @@ impl App {
     // Dictates how entry should be drawn.
     fn drawentry(ui: &mut Ui, subject: &Entry, displayeven: bool) -> (usize, usize) {
         let mut o = (0, 0);
+        ui.with_layout(
+            Layout::left_to_right(egui::Align::Min).with_main_justify(true),
+            |ui| {
+                Frame::new()
+                    .fill(match displayeven {
+                        false => Self::COL_WHITE,
+                        true => Self::COL_GREY10,
+                    })
+                    .corner_radius(4)
+                    .inner_margin(4)
+                    .show(ui, |ui| {
+                        // Switch depending on subject.
+                        if let EntrySwitch::End(v) = &subject.data {
+                            // This is a node of the tree with full data.
+                            ui.with_layout(Layout::left_to_right(egui::Align::Min), |ui| {
+                                // Display full data.
 
-        Frame::new()
-            .fill(match displayeven {
-                false => Self::COL_WHITE,
-                true => Self::COL_GREY10,
-            })
-            .corner_radius(4)
-            .inner_margin(4)
-            .show(ui, |ui| {
-                // Switch depending on subject.
-                if let EntrySwitch::End(v) = &subject.data {
-                    // This is a node of the tree with full data.
-                    ui.with_layout(Layout::left_to_right(egui::Align::Min), |ui| {
-                        // Display full data.
-                        if ui
-                            .button(match v.completed {
-                                None => " ",
-                                Some(_) => "•",
-                            })
-                            .clicked()
-                        {
-                            // When clicked, toggle completed state.
-                            // DAMMIT
+                                let mainbutton = Button::new(match v.completed {
+                                    None => " ",
+                                    Some(_) => "•",
+                                });
+                                if ui.add_sized((20.0, 20.0), mainbutton).clicked() {
+                                    // When clicked, toggle completed state.
+                                    // DAMMIT
+                                }
+
+                                // Add to o.
+                                if let Some(comp) = v.completed {
+                                    o.0 += 1;
+                                    ui.label(comp.format("%d/%m/%Y").to_string());
+                                } else {
+                                    o.1 += 1;
+                                }
+
+                                ui.label(subject.title.to_owned());
+
+                                ui.allocate_space(egui::Vec2 {
+                                    x: ui.available_size().x,
+                                    y: 0.0,
+                                });
+                            });
+                        } else if let EntrySwitch::Host(v) = &subject.data {
+                            // This is a node of the tree with partial data.
+                            ui.with_layout(Layout::top_down(egui::Align::Center), |ui| {
+                                // Draw header with title.
+                                ui.add(Label::new(subject.title.to_owned()));
+
+                                ui.add_space(8.0);
+
+                                // Go recursive using host.
+                                let totals = Self::drawentryhost(ui, v, !displayeven);
+
+                                // Merge pending totals with output totals.
+                                o.0 += totals.0;
+                                o.1 += totals.1;
+
+                                ui.add_space(8.0);
+
+                                // Draw footer in small text.
+                                ui.label("footer");
+                            });
                         }
-
-                        // Add to o.
-                        if let Some(comp) = v.completed {
-                            o.0 += 1;
-                            ui.label(comp.format("%d/%m/%Y").to_string());
-                        } else {
-                            o.1 += 1;
-                        }
-
-                        ui.label(subject.title.to_owned());
                     });
-                } else if let EntrySwitch::Host(v) = &subject.data {
-                    // This is a node of the tree with partial data.
-                    ui.with_layout(Layout::top_down(egui::Align::Min), |ui| {
-                        // Draw header with title.
-                        ui.label(subject.title.to_owned());
-
-                        // Go recursive using host.
-                        let totals = Self::drawentryhost(ui, v, !displayeven);
-
-                        // Merge pending totals with output totals.
-                        o.0 += totals.0;
-                        o.1 += totals.1;
-
-                        // Draw footer in small text.
-                        ui.label("footer");
-                    });
-                }
-            });
+            },
+        );
 
         o
     }
 }
 
 impl eframe::App for App {
+
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+		ctx.set_theme(Theme::Dark);
         // construct navpanel
         egui::SidePanel::left("navigation").show(ctx, |ui| {
-            ui.vertical_centered(|ui| {
+            ui.vertical(|ui| {
                 if ui.button("Table view").clicked() {
                     self.menu = CurrentMenu::TableView;
                 }
                 if ui.button("New entry").clicked() {
                     self.menu = CurrentMenu::Create;
                 };
-                ui.set_width(32.0);
             });
         });
 
         // construct body
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(self.body.clone());
+            //ui.heading(self.body.clone());
             self.tableview(ui);
         });
     }
