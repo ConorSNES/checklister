32b880268f96360f67e819db15b751dede1957a7
Conor <181732605+ConorSS@users.noreply.github.com>
2025-06-07 02:37:29 +0100

    Can now add content entries to root

--- src/app.rs
+++ src/app.rs
@@ -4,26 +4,25 @@ use model::{Entry, EntryHost, EntrySwitch, Model};
 use serde::{Deserialize, Serialize};
 mod model;
 
-#[derive(Serialize, Deserialize)]
-enum CurrentMenu {
-    TableView,
-    Create,
+#[derive(Serialize, Deserialize, PartialEq)]
+enum CurrentDialog {
+    None,
+    Create(Option<Vec<usize>>, String),
+    Edit(Vec<usize>),
 }
 
 // declaration of the application
 #[derive(Serialize, Deserialize)]
 pub struct App {
-    body: String,
     data: Model,
-    menu: CurrentMenu,
+    dialog: CurrentDialog,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            body: "Hello, world".to_owned(),
             data: model::make_sample_set(),
-            menu: CurrentMenu::TableView,
+            dialog: CurrentDialog::None,
         }
     }
 }
@@ -33,6 +32,8 @@ impl App {
     const COL_GREY10: Color32 = Color32::from_rgb(0xF8, 0xF8, 0xF8); */
     const COL_GREYDARK: Color32 = Color32::from_rgb(0x30, 0x30, 0x30);
     const COL_DARK: Color32 = Color32::from_rgb(0x1B, 0x1B, 0x1B);
+
+    const TASK_NAME_DEFAULT: &str = "New Task";
 
     // Constructs view of all elements.
     fn tableview(&mut self, ui: &mut Ui) {
@@ -72,81 +73,202 @@ impl App {
                     // Switch depending on subject.
                     if let EntrySwitch::End(v) = &mut subject.data {
                         // This is a node of the tree with full data.
-                        ui.with_layout(Layout::left_to_right(egui::Align::Min), |ui| {
-                            // Display full data.
+                        Self::drawtriple(
+                            ui,
+                            |ui| {
+                                // Draw the check button. (you can't add onclick events to checkboxes)
+                                if ui
+                                    .add_sized(
+                                        (20.0, 20.0),
+                                        Button::new(match v.completed {
+                                            None => " ",
+                                            Some(_) => "✔",
+                                        }),
+                                    )
+                                    .clicked()
+                                {
+                                    // When clicked, toggle completed state.
+                                    v.completed = match v.completed {
+                                        None => Some(Local::now().naive_local()),
+                                        Some(_) => None,
+                                    };
+                                }
 
-                            let mainbutton = Button::new(match v.completed {
-                                None => " ",
-                                Some(_) => "✔",
-                            });
-                            if ui.add_sized((20.0, 20.0), mainbutton).clicked() {
-                                // When clicked, toggle completed state.
-                                v.completed = match v.completed {
-                                    None => Some(Local::now().naive_local()),
-                                    Some(_) => None,
-                                };
-                            }
-
-                            // Add to o.
-                            if let Some(comp) = v.completed {
-                                o.0 += 1;
-                                ui.label(
-                                    RichText::new(comp.format("%d/%m/%Y").to_string())
-                                        .size(10.0)
-                                        .weak(),
-                                );
-                            } else {
-                                o.1 += 1;
-                            }
-							// hacky solution: draw right elements before left ones to allow filling center. should make a recipe for this
-                            ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                                // Add to completion stats.
+                                // If this entry is complete, add label for date completed.
+                                if let Some(comp) = v.completed {
+                                    o.0 += 1;
+                                    ui.add(Label::new(
+                                        RichText::new(comp.format("%d/%m/%Y").to_string())
+                                            .size(10.0)
+                                            .weak(),
+                                    ));
+                                } else {
+                                    o.1 += 1;
+                                }
+                            },
+                            |ui| {
+                                // Add title.
+                                ui.label(subject.title.to_owned());
+                            },
+                            |ui| {
+                                // Add edit button.
                                 if ui
                                     .add_sized(
                                         [20.0, 20.0],
-                                        Button::new(
-                                            RichText::new("...").weak(),
-                                        )
-                                        .fill(Color32::TRANSPARENT),
+                                        Button::new(RichText::new("...").weak())
+                                            .fill(Color32::TRANSPARENT),
                                     )
                                     .clicked()
-                                {}
-
-                                ui.vertical_centered(|ui| {
-                                    ui.label(subject.title.to_owned());
-                                });
-                            });
-                        });
+                                {
+                                    // This is where you put the switch edit code snoosk
+                                }
+                            },
+                        );
                     } else if let EntrySwitch::Host(v) = &mut subject.data {
                         // This is a node of the tree with partial data.
                         ui.with_layout(Layout::top_down(egui::Align::Min), |ui| {
-                            ui.horizontal_top(|ui| {
-                                // Draw header with title.
-                                ui.add(Label::new(subject.title.to_owned()));
+                            // Draw the header of the box.
+                            Self::drawtriple(
+                                ui,
+                                |ui| {
+                                    ui.add_space(20.0);
+                                },
+                                |ui| {
+                                    // Draw title.
+                                    ui.add(Label::new(subject.title.to_owned()));
+                                },
+                                |ui| {
+                                    if ui.add_sized((20.0, 20.0), Button::new("+")).clicked() {
+                                        // Raise add entry dialog on this entry host
+                                    }
+                                },
+                            );
 
-                                if ui.add_sized((20.0, 20.0), Button::new("+")).clicked() {
-                                    // Raise add entry dialog on this entry host
-                                }
-                            });
+                            ui.add_space(4.0);
 
-                            ui.add_space(8.0);
-
-                            // Go recursive using host.
+                            // Go recursive using inner entry host.
                             let totals = Self::drawentryhost(ui, v, !displayeven);
 
                             // Merge pending totals with output totals.
                             o.0 += totals.0;
                             o.1 += totals.1;
 
-                            ui.add_space(8.0);
+                            ui.add_space(4.0);
 
                             // Draw footer in small text.
-                            ui.label("footer");
+                            ui.horizontal(|ui| {
+                                ui.add_space(8.0);
+                                ui.label(
+                                    RichText::new(format!(
+                                        "{0}/{1} completed",
+                                        totals.0,
+                                        totals.1 + totals.0
+                                    ))
+                                    .weak(),
+                                );
+                            });
                         });
                     }
                 });
         });
 
         o
+    }
+
+    // Constructs the current inline edit dialog.
+    fn dialogview(&mut self, ui: &mut Ui) {
+        if self.dialog == CurrentDialog::None {
+            return;
+        }
+        Frame::new().inner_margin(8).show(ui, |ui| {
+            match &mut self.dialog {
+                CurrentDialog::None => {}
+                CurrentDialog::Create(v, newtaskname) => {
+                    match v {
+                        None => {
+                            ui.heading("Create task");
+                        }
+                        Some(v) => {
+                            ui.heading(format!(
+                                "Create subtask for {}",
+                                self.data.entry.deepget(v).title
+                            ));
+                        }
+                    }
+
+                    ui.text_edit_singleline(newtaskname);
+
+					let mut close: bool = false;
+
+                    ui.horizontal(|ui| {
+                        // Close buttons (with some extra logic to handle the label)
+						let canceled = ui.button("Cancel").clicked();
+						let confirmed = ui.button("Done").clicked();
+
+                        if confirmed {
+                            // add task to location with new content
+                            match v {
+                                None => {
+									// We're adding an entry to the root
+                                    self.data.entry.subelements.push(Entry::new_end(newtaskname.to_string(), "".to_string()));
+									self.data.entry.sort();
+                                }
+                                Some(v) => {
+									// We're adding an entry to a child entry
+									match &mut self.data.entry.deepget(v).data {
+										EntrySwitch::Host(w) => {
+											w.subelements.push(Entry::new_end(newtaskname.to_string(), "".to_string()));
+											w.sort();
+										},
+										EntrySwitch::End(_) => {
+											panic!("Attempted to add an entry to a non-entryhost!");
+										}
+									}
+                                }
+                            };
+                        }
+
+						close = canceled | confirmed;
+                    });
+
+					if close {
+						self.dialog = CurrentDialog::None;
+					}
+                }
+                CurrentDialog::Edit(v) => {
+                    ui.heading("Edit task");
+                    ui.text_edit_singleline(&mut self.data.entry.deepget(v).title);
+                    // Close button
+                    if ui.button("Done").clicked() {
+                        self.dialog = CurrentDialog::None;
+                    }
+                }
+            };
+        });
+    }
+
+    // Recipe for drawing a "triple";
+    // Left, right aligned content with centred content between.
+    fn drawtriple<R>(
+        ui: &mut Ui,
+        left: impl FnOnce(&mut Ui) -> R,
+        centre: impl FnOnce(&mut Ui) -> R,
+        right: impl FnOnce(&mut Ui) -> R,
+    ) {
+        ui.horizontal(|ui| {
+            // Draw left
+            left(ui);
+
+            // Create rtl box
+            ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                // Draw right
+                right(ui);
+
+                // Draw centred box with content
+                ui.vertical_centered(centre);
+            });
+        });
     }
 }
 
@@ -165,18 +287,28 @@ impl eframe::App for App {
         // construct navpanel
         egui::TopBottomPanel::top("navigation").show(ctx, |ui| {
             ui.horizontal_centered(|ui| {
-                if ui.button("Table view").clicked() {
-                    self.menu = CurrentMenu::TableView;
-                }
-                if ui.add_sized((20.0, 20.0), Button::new("+")).clicked() {
-                    // Raise add entry dialog on this entry host
-                }
+                // Wrap these elements in a panel for improved layout
+                Frame::new().inner_margin(4.0).show(ui, |ui| {
+                    if ui.button("Table view").clicked() {
+                        // I don't know what this will do
+                    }
+                    ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                        if ui.add_sized((20.0, 20.0), Button::new("+")).clicked() {
+                            // Raise add entry dialog on this entry host
+                            self.dialog =
+                                CurrentDialog::Create(None, App::TASK_NAME_DEFAULT.to_owned());
+                        }
+                    });
+                });
             });
         });
 
         // construct body
         egui::CentralPanel::default().show(ctx, |ui| {
-            //ui.heading(self.body.clone());
+            // Show current edit modal
+            self.dialogview(ui);
+
+            // Show all tasks
             self.tableview(ui);
         });
     }
--- src/app/model/mod.rs
+++ src/app/model/mod.rs
@@ -1,6 +1,8 @@
 // This file defines the in-memory data model.
 
-use chrono::NaiveDateTime;
+use std::cmp::Ordering;
+
+use chrono::{Local, NaiveDateTime};
 use serde::{Deserialize, Serialize};
 
 pub fn make_sample_set() -> Model {
@@ -35,34 +37,34 @@ pub fn make_sample_set() -> Model {
                     title: "snusklist".to_owned(),
                     data: EntrySwitch::Host(EntryHost {
                         subelements: vec![
-							Entry {
-								title: "subsnusk1".to_owned(),
-								data: EntrySwitch::End(EntryEnd {
-									body: "bjarg".to_owned(),
-									added: NaiveDateTime::default(),
-									completed: Some(NaiveDateTime::default()),
-								}),
-							},
-							Entry {
-								title: "subsnusk2".to_owned(),
-								data: EntrySwitch::End(EntryEnd {
-									body: "bjarg".to_owned(),
-									added: NaiveDateTime::default(),
-									completed: None,
-								}),
-							},
-							Entry {
-								title: "subsnusk3".to_owned(),
-								data: EntrySwitch::End(EntryEnd {
-									body: "bjarg".to_owned(),
-									added: NaiveDateTime::default(),
-									completed: Some(NaiveDateTime::default()),
-								}),
-							}
-						],
+                            Entry {
+                                title: "subsnusk1".to_owned(),
+                                data: EntrySwitch::End(EntryEnd {
+                                    body: "bjarg".to_owned(),
+                                    added: NaiveDateTime::default(),
+                                    completed: Some(NaiveDateTime::default()),
+                                }),
+                            },
+                            Entry {
+                                title: "subsnusk2".to_owned(),
+                                data: EntrySwitch::End(EntryEnd {
+                                    body: "bjarg".to_owned(),
+                                    added: NaiveDateTime::default(),
+                                    completed: None,
+                                }),
+                            },
+                            Entry {
+                                title: "subsnusk3".to_owned(),
+                                data: EntrySwitch::End(EntryEnd {
+                                    body: "bjarg".to_owned(),
+                                    added: NaiveDateTime::default(),
+                                    completed: Some(NaiveDateTime::default()),
+                                }),
+                            },
+                        ],
                     }),
                 },
-				Entry {
+                Entry {
                     title: "snusk4".to_owned(),
                     data: EntrySwitch::End(EntryEnd {
                         body: "bjarg".to_owned(),
@@ -95,6 +97,38 @@ pub struct Entry {
     pub data: EntrySwitch,
 }
 
+impl Entry {
+    // Two functions that construct new entry ends and entry hosts respectively.
+    pub fn new_end(title: String, body: String) -> Self {
+        Entry {
+            title: title,
+            data: EntrySwitch::End(EntryEnd {
+                body: body,
+                added: Local::now().naive_local(),
+                completed: None,
+            }),
+        }
+    }
+
+	// currently dead- need a menu to add a host
+    pub fn _new_host(title: String) -> Self {
+        Entry {
+            title: title,
+            data: EntrySwitch::Host(EntryHost {
+                subelements: vec![],
+            }),
+        }
+    }
+
+	// Collects added date of Entry, regardless of it being a Host or End.
+	pub fn added(&self) -> NaiveDateTime {
+		match &self.data {
+			EntrySwitch::End(v) => v.added,
+			EntrySwitch::Host(v) => v.date()
+		}
+	}
+}
+
 // The main content of an entry.
 #[derive(Serialize, Deserialize)]
 pub struct EntryEnd {
@@ -107,4 +141,56 @@ pub struct EntryEnd {
 #[derive(Serialize, Deserialize, Default)]
 pub struct EntryHost {
     pub subelements: Vec<Entry>,
+}
+
+impl EntryHost {
+    // Function that performs recursive traversal of entry hosts
+    pub fn deepget(&mut self, index: &[usize]) -> &mut Entry {
+        // If the vector is empty, panic
+        if index.len() == 0 {
+            panic!("No indexes provided!")
+        };
+
+        // If this is the most shallow the index goes, return the current item.
+        if index.len() == 1 {
+            &mut self.subelements[index[0]]
+        } 
+		// If this is not, try and access the deeper one
+		else {
+            match &mut self.subelements[index[0]].data {
+                EntrySwitch::End(_) => panic!("Entryhost deepget ended early! Index: {:#?}", index),
+                // Otherwise, go recursive
+                EntrySwitch::Host(v) => v.deepget(&index[1..]),
+            }
+        }
+    }
+
+	// Collect newest date of host
+	pub fn date(&self) -> NaiveDateTime {
+		// basically a max aggregator for one class value, with some fanangling
+		let mut max = NaiveDateTime::MIN;
+		for v in &self.subelements {
+			max = NaiveDateTime::max(max, v.added());
+		};
+		max
+	}
+
+	// Collect newest completed date of host
+	// (it's a little more nuanced than that, but that covers the basic idea)
+	// unf todo
+
+	// Recursive sort for entryhost contents
+	pub fn sort(&mut self) {
+		// Sort the internals if we're able
+		for v in &mut self.subelements {
+			if let EntrySwitch::Host(w) = &mut v.data { w.sort(); }
+		}
+		// Now sort this entry
+		self.subelements.sort_by(|subj1, subj2| { 
+			let s1date = subj1.added();
+			let s2date = subj2.added();
+			if s1date == s2date { return Ordering::Equal };
+			return if !(s1date < s2date) { Ordering::Less } else { Ordering::Greater }
+		});
+	}
 }
