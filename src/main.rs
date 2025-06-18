f39768f6bbe96da23402a1de95a013a91f364a55
Conor <181732605+ConorSS@users.noreply.github.com>
2025-06-18 15:55:07 +0100

    Add icon for window and win builds

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use eframe::egui;
+#![windows_subsystem = "windows"]
+
+use eframe::{egui::{self}, icon_data::from_png_bytes};
 
 mod app;
 
 use app::App;
 
+const ICON: &[u8] = include_bytes!("..\\icon\\32_checklister.png");
+
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_app_id("checklister"),
+        viewport: egui::ViewportBuilder::default()
+			.with_app_id("checklister")
+			.with_icon(from_png_bytes(ICON).expect("Missing Icon File!!")),
         ..Default::default()
     };
 
