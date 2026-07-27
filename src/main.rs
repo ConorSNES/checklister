9194b4f439f9ec0a4671fabe0ba9b348f6ed76f5
ConorSNES <74014503+ConorSNES@users.noreply.github.com>
2026-07-27 17:50:26 +0100

    Fix compile for linux build

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 #![windows_subsystem = "windows"]
 
-use eframe::{egui::{self}, icon_data::from_png_bytes};
+use eframe::{
+    egui::{self},
+    icon_data::from_png_bytes,
+};
 
 mod app;
 
 use app::App;
 
+#[cfg(unix)]
+const ICON: &[u8] = include_bytes!("../icon/32_checklister.png");
+#[cfg(not(unix))]
 const ICON: &[u8] = include_bytes!("..\\icon\\32_checklister.png");
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-			.with_app_id("checklister")
-			.with_icon(from_png_bytes(ICON).expect("Missing Icon File!!")),
+            .with_app_id("checklister")
+            .with_icon(from_png_bytes(ICON).expect("Missing Icon File!!")),
         ..Default::default()
     };
 
-    eframe::run_native("Checklister", options, Box::new(|cc| Ok(Box::new(App::new(cc)))))
+    eframe::run_native(
+        "Checklister",
+        options,
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    )
 }
